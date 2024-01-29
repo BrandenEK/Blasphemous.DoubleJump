09ab7e07018d529d5b6c2603c375f80ac6a1ed2d
unknown <brandenek03912@hotmail.com>
2024-01-28 19:35:03 -0500

    core: create new project files

---
+++ Blasphemous.DoubleJump/JumpController.cs
@@ -0,0 +1,13 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.DoubleJump;
+
+public class JumpController : BlasMod
+{
+    public JumpController() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        LogError($"{ModInfo.MOD_NAME} has been initialized");
+    }
+}
---
+++ Blasphemous.DoubleJump/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.DoubleJump;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "2.0.1")]
+public class Main : BaseUnityPlugin
+{
+    public static JumpController JumpController { get; private set; }
+
+    private void Start()
+    {
+        JumpController = new JumpController();
+    }
+}
