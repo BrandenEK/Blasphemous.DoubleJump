9c2225e80332adddbae782e5cd0ef5c6a4c09977
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-23 18:22:34 -0400

    core: upgrade packages

--- Blasphemous.DoubleJump/Main.cs
+++ Blasphemous.DoubleJump/Main.cs
@@ -3,9 +3,9 @@
 namespace Blasphemous.DoubleJump;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "2.1.0")]
-[BepInDependency("Blasphemous.Framework.Items", "0.1.0")]
-[BepInDependency("Blasphemous.Framework.Levels", "0.1.0")]
+[BepInDependency("Blasphemous.ModdingAPI", "2.4.1")]
+[BepInDependency("Blasphemous.Framework.Items", "0.1.1")]
+[BepInDependency("Blasphemous.Framework.Levels", "0.1.4")]
 internal class Main : BaseUnityPlugin
 {
     public static JumpController JumpController { get; private set; }
